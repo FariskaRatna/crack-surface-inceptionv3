d0bd38cf51ee973b4a315be0d3fcf9d8ff593ada
Fariska Ratna <fariskafauziah07@students.unnes.ac.id>
2024-07-12 11:10:23 +0700

    update converter

--- convert_onnx.py
+++ convert_onnx.py
@@ -1,14 +1,26 @@
 import torch
-import convert_onnx
+import onnx
 from scripts.crack_module import Classifier
+from scripts.inception import InceptionCustom
 
 def main():
-    model = Classifier(n_classes=2)
-    model.load_state_dict(torch.load(""))
+    num_classes = 2
+    inception_model = InceptionCustom(num_classes=num_classes)
+    model = Classifier(net=inception_model, lr=0.001)
+    model.load_state_dict(torch.load("./model/inception_model_crack.pth"))
     model.eval()
 
     dummy_input = torch.randn(1, 3, 299, 299)
 
+    with torch.no_grad():
+        torch.onnx.export(
+            model,
+            dummy_input,
+            "./Crack_inception.onnx",
+            input_names=["input"],
+            output_names=["output"],
+            verbose=True,
+        )
 
 if __name__ == "__main__":
     main()
